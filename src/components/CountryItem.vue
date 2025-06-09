70e12181499f2e52cc43852094c69ad9eba21c53
Alvarows20 <alvarojpt04.ws@gmail.com>
2025-06-09 11:13:34 -0500

    Modified tailwind file and Country Item position

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -18,19 +18,18 @@ defineProps<Props>();
   </div>
   <h4 class="text-xl font-bold text-gray-500">{{ country.name.common }}</h4>
   <p class="text-sm text-gray-400">
-    Capital:
-    <span class="font-bold" v-for="capital in country.capital">
-      {{ capital }}
-      <template>, </template>
-    </span>
-  </p>
-
-  <p class="text-sm text-gray-400">
     Population:
     <span class="font-bold">{{ country.population?.toLocaleString() }}</span>
   </p>
   <p class="text-sm text-gray-400">
     Region:
     <span class="font-bold">{{ country.region }}</span>
+  </p>
+  <p class="text-sm text-gray-400">
+    Capital:
+    <span class="font-bold" v-for="capital in country.capital">
+      {{ capital }}
+      <template>, </template>
+    </span>
   </p>
 </template>
